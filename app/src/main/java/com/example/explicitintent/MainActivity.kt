f5db9bf0c28d2def9e75969f9855606691670827
Mitesh9773 <rathodmeet9153@gmail.com>
2024-07-19 20:02:39 +0530

    Adding files of the project

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnIntent=findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener(){
+
+            intent= Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+
+
+}
